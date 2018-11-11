a4fa8fff6f1429281fb1162390d2edb77e6534aa
yangkai <408786028@qq.com>
2018-11-11 20:24:48 +0800

    20181111 20:24.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-
-Vue.config.productionTip = false
+import App from './App.vue'
+import './index.css'
 
 /* eslint-disable no-new */
 new Vue({
--- src/utils/storageUtils.js
+++ src/utils/storageUtils.js
@@ -1,7 +1,7 @@
 const TODOS_KEY = 'todos_key'
 export  default{
   saveTodos(todos){
-    localStorage.setItem(TODOS_KEY, Json.stringify(todos))
+    localStorage.setItem(TODOS_KEY, JSON.stringify(todos))
   },
   readTodos(){
     return JSON.parse(localStorage.getItem(TODOS_KEY) || '[]')
